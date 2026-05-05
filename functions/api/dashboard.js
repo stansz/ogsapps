c3d0457a29724b22a89f1e38565d6501d770e9bf
Jean Claw <oatclaw88@gmail.com>
2026-05-05 21:57:41 +0000

    fix: switch Business feed to search-based RSS (topic ID returns 404 from CF Worker)

--- functions/api/dashboard.js
+++ functions/api/dashboard.js
@@ -60,7 +60,7 @@ async function fetchWeather(city) {
 const NEWS_FEEDS = [
   { name: 'World', url: 'https://www.cbc.ca/webfeed/rss/rss-topstories' },
   { name: 'Technology', url: 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGRqTVhZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen' },
-  { name: 'Business', url: 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0BtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen' },
+  { name: 'Business', url: 'https://news.google.com/rss/search?q=business+finance+markets&hl=en-US&gl=US&ceid=US%3Aen' },
   { name: 'Canada & BC', url: 'https://news.google.com/rss/search?q=site:cbc.ca+OR+site:globalnews.ca+british+columbia&hl=en-CA&gl=CA&ceid=CA%3Aen' },
   { name: 'Local', urls: [
     'https://dailyhive.com/feed/vancouver',
